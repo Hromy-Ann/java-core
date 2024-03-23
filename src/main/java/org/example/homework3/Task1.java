fb6b90394d15bdefea4f7700b5331f9304f5ee89
Anna Hromova <hromovaannaa@gmail.com>
2024-03-23 22:49:05 +0200

    doing homework3
    task2 is given by two variants

---
+++ src/main/java/org/example/homework3/Task1.java
@@ -0,0 +1,12 @@
+package org.example.homework3;
+
+public class Task1 {
+    public static void main(String[] args) {
+        int a = 3;
+        int b = 7;
+        System.out.println("sum = " + (a + b));
+        System.out.println("difference = " + (a - b));
+        System.out.println("product = " + (a * b));
+        System.out.println("ratio = " + (((float) a) / b));
+    }
+}
